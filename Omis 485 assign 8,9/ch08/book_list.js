5cccffdc794ead4eae396bbdad9a822037ec5d04
Divyadommaraju <47014242+Divyadommaraju@users.noreply.github.com>
2019-03-31 18:32:53 -0500

    Add files via upload

---
+++ Omis 485 assign 8,9/ch08/book_list.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $("#categories h2").click(
+       	function() {
+            $(this).toggleClass("minus");
+            if ($(this).attr("class") !== "minus") {
+                $(this).next().hide();
+            }
+            else {
+                $(this).next().show();
+            }
+
+            $("#image").attr("src", "");   	
+        }
+    ); // end click
+
+    $("#web_images a, #java_images a, #net_images a, #database_images a").each(function() {       
+        var imageURL = $(this).attr("href");
+
+        // preload images		
+        var bookImage = new Image();
+        bookImage.src = imageURL;
+
+        // set up event handlers			
+        $(this).click(function(evt) {
+            $("#image").attr("src", imageURL);
+        
+        evt.preventDefault();
+        }); // end click  
+      }); // end each   
+}); // end ready
